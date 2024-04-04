7b2d8b798454d60bdd827785615262fcf517e933
drezzyts <drezzydev@gmail.com>
2024-04-04 19:01:37 -0300

    feat(utils): implementing a method to get command author

--- src/structs/Utils.ts
+++ src/structs/Utils.ts
@@ -1,10 +1,11 @@
-import { InteractionResponse, Message } from "discord.js";
+import { InteractionResponse, Message, User } from "discord.js";
 import { CommandContext, CommandOption, InteractionCommandContext, PrefixedCommandContext } from "../types/command";
 
 export interface CommandUtils<T extends CommandOption[]> {
   isInteractionContext(context: CommandContext<T>): context is InteractionCommandContext<T>;
   isPrefixedContext(context: CommandContext<T>): context is PrefixedCommandContext<T>;
   reply(content: string): Promise<InteractionResponse<boolean>> | Promise<Message<boolean>>;
+  author(): User;
 }
 
 export class Utils<T extends CommandOption[]> implements CommandUtils<T> {
@@ -22,5 +23,11 @@ export class Utils<T extends CommandOption[]> implements CommandUtils<T> {
     if (this.isInteractionContext(this.context)) 
       return this.context.interaction.reply(content);
     return this.context.message.reply(content);
+  }
+
+  public author(): User {
+    if (this.isInteractionContext(this.context))
+      return this.context.interaction.user;
+    return this.context.message.author;
   }
 }
